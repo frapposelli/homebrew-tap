981365a39034594b94065a5c88ed592b6e12de80
Fabio Rapposelli <fabio@rapposelli.org>
2018-05-14 09:39:27 +0200

    Brew formula update for wwhrd version v0.2.1

--- Formula/wwhrd.rb
+++ Formula/wwhrd.rb
@@ -1,9 +1,9 @@
 class Wwhrd < Formula
   desc "Have Henry Rollins check vendored licenses in your Go project."
   homepage "https://github.com/frapposelli/wwhrd/blob/master/README.md"
-  url "https://github.com/frapposelli/wwhrd/releases/download/v0.2.0/wwhrd_0.2.0_darwin_amd64.tar.gz"
-  version "0.2.0"
-  sha256 "2e4802a79cb7c48249d22842e17b22701823a343461f2ee09f11aa1503c7e9f1"
+  url "https://github.com/frapposelli/wwhrd/releases/download/v0.2.1/wwhrd_0.2.1_darwin_amd64.tar.gz"
+  version "0.2.1"
+  sha256 "7100508e213bca81ffa5af3fe70998b55201481a050c1e6bccfd18ea93be38c9"
 
   def install
     bin.install "wwhrd"
